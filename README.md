97943689ada4551ed8819e86eca2f204cb2eab2e
Archit Gupta <archit@accelbread.com>
2025-02-16 22:34:29 -0800

    Add json read/write

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # zig-io-benchmark
 
 Benchmark application to compare performance for Zig std readers/writers.
+
+As of right now just pretty prints a JSON file using `std.json`.
+
+Steps to test:
+
+```sh
+# baseline
+zig build --release
+# modified std lib
+zig build --release --zig-lib-dir ../zig/lib/ -p zig-out-test
+# compare
+sudo poop -d 600000 \
+  './zig-out/bin/io-benchmark json-data/large-file.json out.json' \
+  './zig-out-test/bin/io-benchmark json-data/large-file.json out.json'
+```
+
+Feel free to open issues/PRs for suggestions/etc.
--- src/main.zig
+++ src/main.zig
@@ -4,4 +4,25 @@
 
 const std = @import("std");
 
-pub fn main() !void {}
+pub fn main() !void {
+    std.debug.assert(std.os.argv.len == 3);
+    var input = try std.fs.cwd().openFileZ(std.os.argv[1], .{});
+    var output = try std.fs.cwd().createFileZ(std.os.argv[2], .{});
+
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+
+    var reader = std.json.Reader(4096, std.fs.File.Reader)
+        .init(arena.allocator(), input.reader());
+    const value = (try std.json.parseFromTokenSource(
+        std.json.Value,
+        arena.allocator(),
+        &reader,
+        .{},
+    )).value;
+
+    try std.json.stringify(
+        value,
+        .{ .whitespace = .indent_2 },
+        output.writer(),
+    );
+}
